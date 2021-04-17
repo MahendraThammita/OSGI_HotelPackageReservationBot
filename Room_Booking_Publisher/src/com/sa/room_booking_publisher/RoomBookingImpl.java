d26378a5441269793dafb38464285393768d5bd4
akila liyanage <mlakilaliyanage@gmail.com>
2021-04-17 14:11:02 +0530

    methods implementation

--- Room_Booking_Publisher/src/com/sa/room_booking_publisher/RoomBookingImpl.java
+++ Room_Booking_Publisher/src/com/sa/room_booking_publisher/RoomBookingImpl.java
@@ -3,11 +3,18 @@
  */
 package com.sa.room_booking_publisher;
 
+import java.util.Scanner;
+
 /**
  * @author akila liyanage
  *
  */
 public class RoomBookingImpl implements IRoomBooking{
+	
+	//variables
+	private String username,checkinDate,checkoutDate;
+	private int headCount,nightsCount,childrenCount;
+	Scanner scanner = new Scanner(System.in);  // Create a Scanner object
 	
 	public RoomBookingImpl() {
 		// TODO Auto-generated constructor stub
@@ -19,6 +26,8 @@ public class RoomBookingImpl implements IRoomBooking{
 	public RoomBookingImpl getUserName() {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Please enter the username : ");
+	    this.username = scanner.nextLine();  // Read user input
 		return this;
 		
 	}
@@ -27,6 +36,8 @@ public class RoomBookingImpl implements IRoomBooking{
 	public RoomBookingImpl headCount() {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Please enter the head count : ");
+	    this.headCount = scanner.nextInt();  // Read user input
 		return this;
 	}
 
@@ -34,6 +45,8 @@ public class RoomBookingImpl implements IRoomBooking{
 	public RoomBookingImpl getNightCount() {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Please enter the night count : ");
+	    this.nightsCount = scanner.nextInt();  // Read user input
 		return this;
 	}
 
@@ -41,6 +54,8 @@ public class RoomBookingImpl implements IRoomBooking{
 	public RoomBookingImpl getChildrenCount() {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Please enter the number of children : ");
+	    this.childrenCount = scanner.nextInt();  // Read user input
 		return this;
 	}
 
@@ -48,6 +63,8 @@ public class RoomBookingImpl implements IRoomBooking{
 	public RoomBookingImpl checkInDate() {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Please enter the check in date : ");
+	    this.checkinDate = scanner.nextLine();  // Read user input
 		return this;
 	}
 
@@ -63,7 +80,7 @@ public class RoomBookingImpl implements IRoomBooking{
 		// TODO Auto-generated method stub
 		//channing the methods
 		
-		new RoomBookingImpl().getUserName().headCount().getChildrenCount().checkInDate().checkOutDate();
+		new RoomBookingImpl().getUserName().headCount().getNightCount().getChildrenCount().checkInDate().checkOutDate();
 		
 	}
 
