230ce5a3fc3ec488d8e4f37807f62b7ce32b76f4
UDITHA J <udithaj.98@gmail.com>
2021-04-18 14:01:20 +0530

    user handler added

---
+++ User_Handling_Publisher/src/user_handling_publisher/Activator.java
@@ -0,0 +1,27 @@
+package user_handling_publisher;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+
+public class Activator implements BundleActivator {
+	
+	ServiceRegistration userhServiceRegistration;
+
+
+
+	public void start(BundleContext context) throws Exception {
+		
+        UserHandler userHandler = new UserHandleraImpl();
+		
+		userhServiceRegistration = context.registerService(UserHandler.class.getName(),userHandler, null);
+		
+	
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		
+	}
+
+}
